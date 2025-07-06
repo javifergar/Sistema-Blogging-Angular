bc64e8e87ab0977d2717a003d710e694724eee02
Javifergar <javierfernandezgarcia123@gmail.com>
2025-07-06 17:43:29 +0200

    Primer commit del codigo. Inputs creados y funcionalidad basica

--- Blog/src/app/components/blog/blog.component.ts
+++ Blog/src/app/components/blog/blog.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { Inoticias } from '../../interfaces/inoticias';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.css'
+  styleUrl: './blog.component.css',
 })
 export class BlogComponent {
+  noticia: Inoticias = {
+    titulo: '',
+    imagen: '',
+    texto: '',
+    fecha: '',
+  };
+  arrayNoticias: Inoticias[] = [];
 
+  cargarNoticia() {
+    this.arrayNoticias.push(this.noticia);
+    this.noticia = { titulo: '', imagen: '', texto: '', fecha: '' };
+    console.log(this.arrayNoticias);
+  }
+  pintarNoticia() {
+    let html = '';
+    this.arrayNoticias.forEach((noticia) => {
+      html += `<li class="rojo">${noticia.titulo} ${noticia.imagen} ${noticia.texto} ${noticia.fecha}</li>`;
+    });
+    return html;
+  }
 }
